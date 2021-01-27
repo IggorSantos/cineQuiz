a301aae985b4a0a5ee77dbf728918c111537d025
Igor Santos <igor.sagitarius@gmail.com>
2021-01-27 20:47:22 -0300

    Estilização e implementação da pagina de quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -24,42 +24,75 @@ function LoadingWidget() {
   );
 }
 
+function QuestionWidget({
+  question,
+  totalQuestions,
+  questionIndex,
+ }) {
+  const questionId = `question__${questionIndex}`
+  return(
+    <Widget>
+     <Widget.Header>
+       <h3>
+        {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+     </Widget.Header>
+     <img
+      alt="Descrição"
+      style={{
+       width: '100%',
+       height: '150px',
+       objectFit: 'cover',
+      }}
+      src={question.image}
+     />
+     <Widget.Content>
+       <h2>
+        {question.title}
+       </h2>
+       <p>
+        {question.description}
+       </p>
+       <form>
+        {question.alternatives.map((alternative, alternativeIndex) => {
+          const alternativeId = `alternative__${alternativeIndex}`
+          return(
+           <Widget.Topic
+              as="label"
+              htmlFor={alternativeId}
+            >
+            <input
+              //style={{ display: 'none'}}
+              id={alternativeId}
+              name={questionId}
+              type="radio"
+            />
+            {alternative}
+           </Widget.Topic>
+          )
+        })}
+       <Button>
+        Confirmar
+       </Button>
+      </form>
+     </Widget.Content>
+    </Widget>
+  )
+}
+
 export default function Home() {
-  console.log(db.questions)
+  const totalQuestions = db.questions.length
+  const questionIndex = 0
+  const question = db.questions[questionIndex]
   return (
     <QuizBackground backgroundImage={db.bg}>
      <QuizContainer>
       <QuizLogo />
-       <Widget>
-        <Widget.Header>
-          <h3>
-           Pergunta
-           1
-           de
-           {`${db.questions.length}`}
-           </h3>
-        </Widget.Header>
-        <img
-         alt="Descrição"
-         style={{
-          width: '100%',
-          height: '150px',
-          objectFit: 'cover',
-         }}
-         src="https://placehold.it/400x400"
+       <QuestionWidget
+          question={question}
+          questionIndex={questionIndex}
+          totalQuestions={totalQuestions}
         />
-        <Widget.Content>
-          <h2>
-           Titulo
-          </h2>
-          <p>
-           Descrição
-          </p>
-          <Button>
-           Confirmar
-          </Button>
-        </Widget.Content>
-       </Widget>
        <LoadingWidget />
        <Footer />
      </QuizContainer>
