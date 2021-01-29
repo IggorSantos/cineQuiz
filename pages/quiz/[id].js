520efe9b210c654966e60bf68c9bd1979bede0ef
Igor Santos <igor.sagitarius@gmail.com>
2021-01-29 17:41:54 -0300

    Construção da pagina dos quizes da galera

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -6,3 +6,23 @@ export default function QuizDaGaleraPage(){
     </div>
   )
 }
+
+export async function getServerSideProps(context){
+  const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
+        }
+        throw new Error('Falha em pegar os dados');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+      .catch((err) => {
+      console.error(err);
+      });
+
+    // console.log('dbExterno', dbExterno);
+    // console.log('Infos que o Next da para nós', context.query.id);
+  return{
+    props:{}
+  }
+}
